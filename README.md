95b9f5af59d944ad78e951133e033d4d2b66c861
Michael Nadel <mike@nadel.org>
2024-02-28 16:36:44 -0600

    ripgrep support, native smart case searching

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Simple full-text search for Shimmering Obsidian via `of`.
 1. Install Obsidifred
 1. Install [Shimmering Obsidian](https://github.com/chrisgrieser/shimmering-obsidian)
 1. Configure Obsidifred to specify vault path (using the same as Shimmering Obsidian's)
+
+## Behavior
+
+If you enable [RipGrep](https://github.com/BurntSushi/ripgrep) then `rg` needs to be in your path, and we pass along the `--smart-case` flag.
+
+Otherwise, we'll do Python-native smart case searching. If you're doing a case sensitive search we'll open the file, read its contents, convert to lowercase, and search for your term. If you're doing a case insensitive search we'll read the file via `mmap` to find your term. 
+
--- search.py
+++ search.py
@@ -4,38 +4,54 @@ import os
 import sys
 import mmap
 import json
+import subprocess
 
-case_sensitive = "case_sensitive" in os.environ
+use_ripgrep = "ripgrep" in os.environ
 basedir = os.environ["vault_path"]
-needle = str.encode(" ".join(sys.argv[1:])) if case_sensitive else " ".join(sys.argv[1:])
+term = " ".join(sys.argv[1:])
+case_sensitive = term.lower() != term # use smart case searching (if there's an uppercase letter then it's case sensitive)
+needle = str.encode(term) if case_sensitive else term
 
 if "debug" in os.environ:
 	print("searching", basedir, "for", needle, file=sys.stderr)
 
 match_list = []
 
-def matches(fpath):
+def contains(fpath, term):
 	if os.path.getsize(fpath) < 1:
 		return False
 
 	with open(fpath, mode="r", encoding="utf-8") as f:
 		if case_sensitive:
 			s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-			return s.find(needle) != -1
+			return s.find(term) != -1
 		else:
-			return needle.lower() in f.read().lower()
+			return term.lower() in f.read().lower()
 
-
-for root, dirs, files in os.walk(basedir):
-	for file in files:
-		if file.endswith(".md"):
-			fpath = os.path.join(root, file)
-			if matches(fpath):
-				relpath = root[len(basedir)+1:]
-				match_list.append({
-					"title": file[:-3],
-					"subtitle": relpath,
-					"arg": os.path.join("./", relpath, file)
-				})
+if use_ripgrep:
+	p = subprocess.run(["rg", "-lS", needle, basedir], capture_output=True)
+	if p.returncode != 0:
+		sys.exit(p.returncode)
+	for file in [x for x in p.stdout.decode().split("\n") if len(x) > 0]:
+		dirname, filename = os.path.split(file)
+		relpath = file.removeprefix(basedir)[1:]
+		relintermediate = relpath.removesuffix(filename)[:-1]
+		match_list.append({
+			"title": filename[:-3],
+			"subtitle": relintermediate,
+			"arg": os.path.join("./", relpath)
+		})
+else:
+	for root, dirs, files in os.walk(basedir):
+		for file in files:
+			if file.endswith(".md"):
+				fpath = os.path.join(root, file)
+				if contains(fpath, needle):
+					relpath = root[len(basedir)+1:]
+					match_list.append({
+						"title": file[:-3],
+						"subtitle": relpath,
+						"arg": os.path.join("./", relpath, file)
+					})
 
 print(json.dumps({"items": match_list}))
