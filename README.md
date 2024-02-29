375f124904ec4b150d7fb2cbdd11dddaca087ec4
Michael Nadel <mike@nadel.org>
2024-02-28 18:28:03 -0600

    refactored to be (slightly) more pythonic

--- README.md
+++ README.md
@@ -13,4 +13,3 @@ Simple full-text search for Shimmering Obsidian via `of`.
 If you enable [RipGrep](https://github.com/BurntSushi/ripgrep) then `rg` needs to be in your path, and we pass along the `--smart-case` flag.
 
 Otherwise, we'll do Python-native smart case searching. If you're doing a case sensitive search we'll open the file, read its contents, convert to lowercase, and search for your term. If you're doing a case insensitive search we'll read the file via `mmap` to find your term. 
-
--- search.py
+++ search.py
@@ -15,7 +15,23 @@ needle = str.encode(term) if case_sensitive else term
 if "debug" in os.environ:
 	print("searching", basedir, "for", needle, file=sys.stderr)
 
-match_list = []
+def ripgrep_search(basedir, needle):
+	match_list = []
+
+	p = subprocess.run(["rg", "-lS", needle, basedir], capture_output=True)
+	if p.returncode != 0:
+		sys.exit(p.returncode)
+	for file in [x for x in p.stdout.decode().split("\n") if x.lower().endswith(".md")]:
+		_, filename = os.path.split(file)
+		relpath = file.removeprefix(basedir)[1:]
+		intermediate = relpath.removesuffix(filename)[:-1]
+		match_list.append({
+			"title": filename[:-3],
+			"subtitle": intermediate,
+			"arg": os.path.join("./", relpath)
+		})
+
+	return match_list
 
 def contains(fpath, term):
 	if os.path.getsize(fpath) < 1:
@@ -28,23 +44,12 @@ def contains(fpath, term):
 		else:
 			return term.lower() in f.read().lower()
 
-if use_ripgrep:
-	p = subprocess.run(["rg", "-lS", needle, basedir], capture_output=True)
-	if p.returncode != 0:
-		sys.exit(p.returncode)
-	for file in [x for x in p.stdout.decode().split("\n") if len(x) > 0]:
-		dirname, filename = os.path.split(file)
-		relpath = file.removeprefix(basedir)[1:]
-		relintermediate = relpath.removesuffix(filename)[:-1]
-		match_list.append({
-			"title": filename[:-3],
-			"subtitle": relintermediate,
-			"arg": os.path.join("./", relpath)
-		})
-else:
+def native_search(basedir, needle):
+	match_list = []
+
 	for root, dirs, files in os.walk(basedir):
 		for file in files:
-			if file.endswith(".md"):
+			if file.lower().endswith(".md"):
 				fpath = os.path.join(root, file)
 				if contains(fpath, needle):
 					relpath = root[len(basedir)+1:]
@@ -53,5 +58,10 @@ else:
 						"subtitle": relpath,
 						"arg": os.path.join("./", relpath, file)
 					})
+	
+	return match_list
 
-print(json.dumps({"items": match_list}))
+def searcher():
+	return ripgrep_search if use_ripgrep else native_search
+
+print(json.dumps({"items": searcher()(basedir, needle)}))
